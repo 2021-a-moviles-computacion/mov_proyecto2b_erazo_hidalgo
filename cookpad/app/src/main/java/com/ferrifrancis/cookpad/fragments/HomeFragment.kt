36a6fa2bc9f7bb69d7ce8a19a28c30e6a3fa7bc0
María Cristina <maria.erazo@epn.edu.ec>
2021-08-24 19:41:44 -0500

    Ver Receta v1

--- cookpad/app/src/main/java/com/ferrifrancis/cookpad/fragments/HomeFragment.kt
+++ cookpad/app/src/main/java/com/ferrifrancis/cookpad/fragments/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.ferrifrancis.cookpad.fragments
 
 import android.os.Bundle
-import android.view.ContextMenu
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +10,6 @@ import com.ferrifrancis.cookpad.R
 import com.ferrifrancis.cookpad.data.Data
 import com.ferrifrancis.cookpad.adapter.HomeRecyclerAdapter
 import kotlinx.android.synthetic.main.fragment_home.*
-import kotlinx.android.synthetic.main.layout_home_list_item.*
 
 
 class HomeFragment : Fragment() {
